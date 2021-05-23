725710b09bae1b9813a12372b4cb958d082ab79c
kriss92 <kristija.lukacek7@gmail.com>
2021-05-23 16:10:07 +0200

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,33 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Camera } from '@ionic-native/camera/ngx';
-import { IonicStorageModule } from '@ionic/storage';
+import { Camera } from '@ionic-native/camera/ngx';//odrađeno
+import { IonicStorageModule } from '@ionic/storage';//odrađeno
+import { Geolocation } from '@ionic-native/geolocation/ngx';//odrađeno
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';//odrađeno
+import { Network } from '@ionic-native/network/ngx';//odrađeno
+import { AlertController } from '@ionic/angular';
+import { Clipboard } from '@ionic-native/clipboard/ngx';//odrađeno
+//import { HttpModule } from '@angular/http';
+import { HTTP } from '@ionic-native/http/ngx'
+//import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  imports: [BrowserModule, /*HttpClientModule, HttpModule*/, IonicModule.forRoot(), AppRoutingModule,
     IonicStorageModule.forRoot()
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Camera,
+    Geolocation,
+    NativeGeocoder,
+    Network,
+    AlertController,
+    Clipboard,
+    HTTP,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
